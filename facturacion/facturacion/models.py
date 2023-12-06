4f3a0faf6a1db275230480b03dccafee3a05f47e
sebastian ramirez <sebastramireza@gmail.com>
2023-12-06 01:16:16 -0500

    models de nuevo

--- facturacion/facturacion/models.py
+++ facturacion/facturacion/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Facturacion(models.Model):
+    id_factura = models.IntegerField(primary_key=True)
+    cedula_paciente = models.IntegerField()
+    objetos_factura = models.TextField()  # Puedes almacenar la lista de objetos como un texto, por ejemplo JSON
+    costo_total = models.IntegerField()
+    iva = models.IntegerField()
+
+    def __str__(self):
+        return f"Factura #{self.id_factura} - Paciente: {self.cedula_paciente}"
 
